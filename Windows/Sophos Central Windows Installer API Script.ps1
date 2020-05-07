3862fd0645268d3c27f06b8f3772949d69510b18
q2srw77 <stephenweber@gmail.com>
2020-05-07 11:25:39 -0400

    Initial commit

---
+++ Windows/Sophos Central Windows Installer API Script.ps1
@@ -0,0 +1,135 @@
+# -----------------------------------------------------------------------------------------------
+# Component: Sophos Central Installer
+# Author: Stephen Weber
+# Purpose: Using the new Sophos Thin installer, 
+#          perform default install of Sophos Central using the defined variables
+# Version 1.2
+# -----------------------------------------------------------------------------------------------
+
+#Setup Customer Variables
+#CustomerToken - Example - "Customer Token Here"
+#Products - Example - "antivirus,intercept"
+$Global:CustomerToken = 
+$Global:Products = 
+
+# Define Functions
+
+function Get-SophosInstalled {
+
+$Global:installed = (gp HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*).DisplayName -contains "Sophos Endpoint Agent"
+$Global:mcsclient = Get-Service -name "Sophos MCS Client" -ea SilentlyContinue
+$Global:mcsagent = Get-Service -name "Sophos MCS Agent" -ea SilentlyContinue
+}
+
+# Sophos Central Installation
+Start-Transcript c:\temp\SophosCentralInstallLog.txt
+Write-Host "Starting the Sophos Central Installation based on the variables defined in the site"
+Write-Host ""
+Write-Host "Checking to see if Sophos is Already Installed"
+
+Get-SophosInstalled
+if ($installed -eq "True") {
+	Write-Host "--Sophos Central Endpoint Agent Installed"
+	if ($mcsclient.Status -eq "Running"){	
+	Write-Host "--Sophos MCS Client is Running"
+	Exit 0
+	}
+}
+else {
+	Write-Host "--Sophos Central is Not Installed"
+	Write-Host "Sophos MCS Client is Not Running"
+	}
+
+# Check for the Site Variables
+Write-Host ""
+Write-Host "Checking the Variables"
+
+if ($CustomerToken -eq $null)
+	{Write-Host "--Customer Token Not Set or Missing"
+    Stop-Transcript
+	Exit 1}
+else
+	{Write-Host "--CustomerToken = "$CustomerToken""}
+
+if ($Products -eq $null)
+	{Write-Host "--Sophos Products Not Set or Missing"
+	Stop-Transcript
+	Exit 1}
+else
+	{Write-Host "--Products = "$Products""}
+
+# Sophos parameters are defined from the site specific variables
+$arguments = "--products=""" + $Products
+$arguments = $arguments + """ --quiet"
+
+# Check to see if a previous SophosSetup Process is running
+Write-Host ""
+Write-Host "Checking to see if SophosSetup.exe is already running"
+if ((get-process "sophossetup" -ea SilentlyContinue) -eq $Null){
+        Write-Host "--SophosSetup Not Running" 
+}
+else {
+    Write-Host "Sophos Currently Running, Will Kill the Process before Continuing"
+    Stop-Process -processname "sophossetup"
+ }
+
+# Download of the Central Customer Installer
+Write-Host ""
+Write-Host "Downloading Sophos Central Installer"
+Invoke-WebRequest -Uri "https://central.sophos.com/api/partners/download/windows/v1/$CustomerToken/SophosSetup.exe" -OutFile SophosSetup.exe
+if ((Test-Path SophosSetup.exe) -eq "True"){
+		Write-Host "--Sophos Setup Installer Downloaded Successfully"
+}
+else {
+	Write-Host "--Sophos Central Installer Did Not Download - Please check Firewall or Web Filter"
+	Stop-Transcript
+	Exit 1
+}
+
+# This Section starts the installer using the arguments defined above
+Write-Host ""
+Write-Host "Installing Sophos Central Endpoint:"
+Write-Host ""
+Write-Host "SophosSetup.exe "$arguments""
+Write-Host ""
+
+start-process SophosSetup.exe $arguments
+
+$timeout = new-timespan -Minutes 30
+$install = [diagnostics.stopwatch]::StartNew()
+while ($install.elapsed -lt $timeout){
+    if ((Get-Service "Sophos MCS Client" -ea SilentlyContinue)){
+	Write-Host "Sophos MCS Client Found - Breaking the Loop"
+	Break
+	}
+    start-sleep -seconds 60
+}
+Write-Host ""
+Write-Host "Sophos Setup Completed"
+
+# Verify that Sophos Central Endpoint Agent Installed
+Write-Host ""
+Write-Host "Verifying that Sophos Central Endpoint installed and is Running"
+
+Get-SophosInstalled
+if ($installed -eq "True") {
+	Write-Host "--Sophos Central Endpoint Agent Installed Successfully"
+	if ($mcsclient.Status -eq "Running"){
+	Write-Host "--Sophos MCS Client is Running"
+		if ($mcsagent.Status -eq "Running"){
+		Write-Host "--Sophos MCS Agent is Running"
+		Write-Host "Log Location - <system>\programdata\Sophos\Cloudinstaller\Logs\"
+		Stop-Transcript
+		Exit 0
+		}
+	}
+}
+else {
+	Write-Host "--Sophos Central Install Failed"
+	Write-Host ""
+	Write-Host "Please check the Sophos Central Install Logs for more details"
+	Write-Host ""
+	Write-Host "Log Location - <system>\programdata\Sophos\Cloudinstaller\Logs\"
+	Stop-Transcript
+	Exit 1
+	}
---
+++ macOS/Sophos Central macOS Install Script.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------------------------
+# Component: Sophos Central Installation
+# Author: Stephen Weber
+# Purpose: macOS Installation for Sophos Central Endpoints
+# Version 1.0
+# -----------------------------------------------------------------------------------------------
+
+#Setup Variables
+
+SophosCustToken="Customer Token Here"
+	#Customer Token from CSV
+SophosMgmtServer_macOS="macOS Mgmt Server Location Here"
+	#macOS Mgmt Server Location from CSV
+SophosEndpointSelection="antivirus intercept" 
+	#Options:
+		#Central Endpoint Protection: "antivirus"
+		#Central Intercept X Advanced: "antivirus intercept"
+		#Central Intercept X Adv with MTR: "antivirus intercept mdr"
+SophosDeviceEncryption="false"
+	#Options: true or false
+
+#Setup Functions
+
+SophosInstalled(){
+if pgrep SophosMcsAgentD; then MCSstatus="Protected"; else MCSstatus="NotProtected"; fi
+
+if pgrep SophosAutoUpdate; then AUstatus="Protected"; else AUstatus="NotProtected"; fi
+
+if open -Ra "Sophos Endpoint"; then Sophos="Installed"; else Sophos="NotInstalled"; fi
+}
+
+#Create Log Directory and setup STDOUT to /tmp/SophosCentral/ScriptLog.txt
+
+[ ! -d "/tmp/SophosCentral" ] && mkdir /tmp/SophosCentral || echo "Directory Already Exists"
+
+now=$(date +"%m_%d_%y_%T")
+
+exec > /tmp/SophosCentral/ScriptLog_$now.txt
+
+#Check for the Site Variables
+
+echo ""
+echo "Checking the Site Variables"
+echo ""
+
+if [ -z $SophosCustToken ]
+	then
+		echo "--Customer Token Not Set or Missing"
+		exit 1
+	else
+		echo "--CustomerToken = "$SophosCustToken""
+fi
+
+if [ -z $SophosMgmtServer_macOS ]
+	then
+		echo "--Mgmt Server macOS Not Set or Missing"
+		exit 1
+	else
+		echo "--SophosMgmtServer_macOS = "$SophosMgmtServer_macOS
+fi
+
+#Sophos Central Installed?
+
+echo ""
+echo "Checking to see if Sophos Central is Installed"
+echo ""
+
+SophosInstalled
+if [ $Sophos = "Installed" ]
+	then
+		if [ $MCSstatus = "Protected" ]
+			then
+				if [ $AUstatus = "Protected" ]
+					then
+						echo "Sophos Central Installed"
+						exit 0
+					else
+						echo "Sophos Central Installed: Auto Update Service Not Running"
+						exit 1
+				fi
+			else
+				echo "Sophos Central Installed: Sophos MCS Service Not Running"
+				exit 1
+		fi
+	else
+		echo "Installing Sophos Central"
+fi
+
+#Sophos Product Selection
+
+[ $SophosDeviceEncryption = "true" ] && SophosProducts=$SophosEndpointSelection" deviceEncryption" || SophosProducts=$SophosEndpointSelection
+
+# Sophos Command-Line Options
+
+#[ $QuietInstall = "true" ] && SophosOptions=" --quiet" || SophosOptions=""
+
+#Build the arguments for install
+
+arguments="--customertoken "$SophosCustToken" --mgmtserver "$SophosMgmtServer_macOS" --products "$SophosProducts" --quiet"
+
+# This Section starts the installer using the arguments defined above
+echo ""
+echo "SophosInstall "$arguments""
+echo ""
+
+#Unzip the SophosInstall.zip file
+
+unzip SophosInstall.zip
+
+#Change executable permissions
+
+chmod a+x Sophos\ Installer.app/Contents/MacOS/Sophos\ Installer
+chmod a+x Sophos\ Installer.app/Contents/MacOS/tools/com.sophos.bootstrap.helper
+
+#Start Installation
+
+./Sophos\ Installer.app/Contents/MacOs/Sophos\ Installer $arguments
+
+#Sleep while waiting for the Installation to Complete
+
+sleep 600
+
+#Verify that Sophos Central Endpoint Agent Installed
+
+echo ""
+echo "Verifying that Sophos Central Endpoint installed and is Running"
+echo ""
+
+SophosInstalled
+
+if [ $Sophos = "Installed" ]
+	then
+		if [ $MCSstatus = "Protected" ]
+			then
+				if [ $AUstatus = "Protected" ]
+					then
+						echo "Sophos Central Installed Successfully"
+						exit 0
+					else
+						echo "Sophos Central Installed: Auto Update Service Not Running"
+						exit 1
+				fi
+			else
+				echo "Sophos Central Installed: Sophos MCS Service Not Running"
+				exit 1
+		fi
+	else
+		echo "Sophos Central Installation Failed"
+fi
